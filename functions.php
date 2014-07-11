cb0d7a669107918cec3e727da587be628c5e58fc
Jeff Matson <jeff@jeffmatson.net>
2014-07-11 15:43:18 -0700

    initial commit

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function wp_sms_send_notification( $wp_sms_phone_number, $wp_sms_updated, $wp_sms_updated_item ) {
+        mail ($wp_sms_phone_number, $wp_sms_updated, $wp_sms_updated_item);
+                
+}
+
+$wp_sms_post = get_post();
+$wp_sms_post_ID = $wp_sms_post->ID;
+$wp_sms_post_title = $wp_sms_post->post_title;
+
+add_action( 'transition_post_status', 'detect_published_post', 10, 3 );
+
+function detect_published_post ( $wp_sms_new_status, $wp_sms_old_status, $wp_sms_post_ID ) {
+	if ( $wp_sms_new_status == 'publish' && $wp_sms_old_status != 'publish' ) {
+		wp_sms_send_notification( '9806215740@messaging.sprintpcs.com', 'New Changes to your WordPress site', "This new post has been published: {$wp_sms_post_title}" );	
+	}
+}
+
---
+++ wp-sms.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WP SMS
+ * Plugin URI: http://jeffmatson.net/wp-sms
+ * Description: Send SMS notifications when things are updated.
+ * Version: 0.1
+ * Author: Jeff Matson
+ * Author URI: http://jeffmatson.net
+ * License: GPL2
+ */
+include 'functions.php';
