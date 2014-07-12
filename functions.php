aa2f762a382e346ea45d8e36d87b61dca08e4ce9
Jeff Matson <jeff@jeffmatson.net>
2014-07-12 11:09:59 -0700

    added more notification options

--- functions.php
+++ functions.php
@@ -3,14 +3,51 @@ function wp_sms_send_notification( $wp_sms_phone_number, $wp_sms_updated, $wp_sm
         mail ($wp_sms_phone_number, $wp_sms_updated, $wp_sms_updated_item);
                 
 }
-
-add_action( 'transition_post_status', 'detect_published_post', 10, 3 );
-
 $wp_sms_phone = get_option('wp_sms_phone_number') . get_option('wp_sms_carrier');
 
 
+// Sends SMS when post is published.
+if (get_option('wp_sms_on_post_publish') == '1'){
 function detect_published_post ( $wp_sms_new_status, $wp_sms_old_status, $wp_sms_post_ID ) {
 	if ( $wp_sms_new_status == 'publish' && $wp_sms_old_status != 'publish' ) {
-		wp_sms_send_notification( $GLOBALS['wp_sms_phone'], 'New Changes to your WordPress site', "This new post has been published: {$wp_sms_post_ID->post_title}" );	
+		wp_sms_send_notification( $GLOBALS['wp_sms_phone'], '', "This new post has been published: {$wp_sms_post_ID->post_title}" );	
 	}
 }
+add_action( 'transition_post_status', 'detect_published_post', 10, 3 );
+}
+
+// Sends SMS when a user logs in.
+if (get_option('wp_sms_on_user_login') == '1'){
+if(!function_exists('get_currentuserinfo')) {
+    include(ABSPATH . "wp-includes/pluggable.php"); 
+}
+global $current_user;
+get_currentuserinfo();
+$wp_sms_new_user_logged_in = $current_user->user_login;
+function detect_user_login( $wp_sms_new_user_logged_in ) {
+	wp_sms_send_notification( $GLOBALS['wp_sms_phone'], '', "User successfully logged in: {$wp_sms_new_user_logged_in}");
+}
+add_action('wp_login', 'detect_user_login', 10, 2);
+}
+
+// Sends SMS when a plugin is updated.
+if (get_option('wp_sms_on_plugin_update') == '1') {
+	function wp_sms_plugin_updated( $a, $b, $c ) {
+		if ( $b['type'] == 'plugin' && $b['action'] == 'update'] {
+			wp_sms_send_notification( $GLOBALS['wp_sms_phone'], '', "Plugin has been updated: {$c['destination_name']}");
+		}
+	}
+	add_action('upgrader_post_install', 'wp_sms_plugin_updated', 10, 3);
+}
+
+// Sends SMS when a plugin is installed.
+if (get_option('wp_sms_on_plugin_install') == '1') {
+        function wp_sms_plugin_install( $a, $b, $c ) {
+                if ( $b['type'] == 'plugin' && $b['action'] == 'install'] {
+                        wp_sms_send_notification( $GLOBALS['wp_sms_phone'], '', "Plugin has been installed: {$c['destination_name']}");
+                }
+        }
+        add_action('upgrader_post_install', 'wp_sms_plugin_install', 10, 3);
+}
+
+
--- wp-sms-options.php
+++ wp-sms-options.php
@@ -27,6 +27,9 @@ function update_wp_sms_settings() {
 	register_setting( 'wp_sms_settings', 'wp_sms_on_post_publish' );
 	register_setting( 'wp_sms_settings', 'wp_sms_phone_number' );
 	register_setting( 'wp_sms_settings', 'wp_sms_carrier' );
+	register_setting( 'wp_sms_settings', 'wp_sms_on_user_login' );
+	register_setting( 'wp_sms_settings', 'wp_sms_on_plugin_update' );
+	register_setting( 'wp_sms_settings', 'wp_sms_on_user_install' );
 }
 
 function wp_sms_notifications_menu(){
@@ -36,16 +39,32 @@ function wp_sms_notifications_menu(){
         <form method="post" action="options.php">
         <?php settings_fields( 'wp_sms_settings' ); ?>
 	<?php do_settings_sections( 'wp_sms_settings' ); ?>
-	<table class="wp-sms-form-table">
+	<table class="form-table">
                 <tr valign="top">
 			<th scope="row">Phone number:</th>
                         <td><input type="text" name="wp_sms_phone_number" value="<?php echo get_option('wp_sms_phone_number'); ?>"/></td>
+		</tr>
+		<tr valign="top">
 			<th scope="row">Cell carrier:</th>
-			<select name="wp_sms_carrier">
+			<td><select name="wp_sms_carrier">
 				<option value="@messaging.sprintpcs.com">Sprint</option>
-			</select>
+			</select></td>
+		</tr>
+		<tr valign="top">
                         <th scope="row">Send SMS when a post is published:</th>
                         <td><input type="checkbox" name="wp_sms_on_post_publish" value="1" <?php if (get_option('wp_sms_on_post_publish') == '1') { echo 'checked'; }?>/></td>
+                </tr>
+		<tr valign="top">
+			<th scope="row">Send SMS when user logs in:</th>
+			 <td><input type="checkbox" name="wp_sms_on_user_login" value="1" <?php if (get_option('wp_sms_on_user_login') == '1') { echo 'checked'; }?>/></td>
+		</tr>
+		<tr valign="top">
+                        <th scope="row">Send SMS when a plugin is installed:</th>
+                         <td><input type="checkbox" name="wp_sms_on_plugin_install" value="1" <?php if (get_option('wp_sms_on_plugin_install') == '1') { echo 'checked'; }?>/></td>
+                </tr>
+		<tr valign="top">
+                        <th scope="row">Send SMS a plugin is updated:</th>
+                         <td><input type="checkbox" name="wp_sms_on_plugin_update" value="1" <?php if (get_option('wp_sms_on_plugin_update') == '1') { echo 'checked'; }?>/></td>
                 </tr>
         </table>
 	<?php submit_button(); ?>
